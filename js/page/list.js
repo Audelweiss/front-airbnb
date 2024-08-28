10b9bb1779d420af510e9bf24cde644a03728099
Audrey <Audelweiss@users.noreply.github.com>
2024-08-28 10:24:42 +0200

    Ajout des notes récupérées via API
    - calcul de la moyenne des notes
    - génération du html et gestion si pas de notes

--- js/page/list.js
+++ js/page/list.js
@@ -1,20 +1,79 @@
 "use strict";
-
+/**********
+ * IMPORTS
+ **********/
 import Home from "../class/home.js";
+/**********
+ * VARIABLES
+ **********/
 
 const UL = document.querySelector("#home-list");
+/**********
+ * FONCTIONS
+ **********/
+
+/**
+ * Calcule la note moyenne pour une annonce
+ * @param {object[]} reviews liste des reviews retournées brutes par l'API
+ * @returns {number} Note moyenne pour la maison
+ */
+function avgReviews(reviews) {
+	//sommes des notes
+	const reviewsFormatted = reviews.reduce(
+		(previous, current) => previous + current.attributes.note,
+		0
+	);
+	//retourne la moyenne arrondie à l'entier le plus proche
+	return Math.round(reviewsFormatted / reviews.length);
+}
+
+/**
+ * Génère l'affichage des notes d'une annonce
+ * @param {number} moy
+ * @returns {string} HTML de l'affichage de la note soit sous forme de message soit les étoiles
+ */
+function generateRating(moy) {
+	let html = "<small><i>Pas encore de note</i></small>";
+	if (!isNaN(moy)) {
+		html = "";
+
+		for (let i = 0; i < 5; i++) {
+			console.log(moy > i);
+			html += `<span
+										class="mask mask-star-2 bg-slate-100 w-6 aspect-square inline-block ${
+											moy > i ? "" : "opacity-50"
+										}"
+									></span>`;
+		}
+	}
+	return html;
+}
+
+/**********
+ * CODE PRINCIPAL
+ **********/
 
 fetch("http://localhost:1337/api/homes?populate=*")
 	.then((response) => response.json())
-	.then((homes) => {
-		console.log(homes);
+	.then(async (homes) => {
+		//récupération des notes
+		const reviewsResponse = await fetch(
+			"http://localhost:1337/api/reviews?populate=*"
+		);
+		const reviewsDatas = await reviewsResponse.json();
+
 		//création du tableau avec les objets formatés grâce à la classe Home
 		const formattedHomes = homes.data.map(
 			(home) => new Home({ ...home.attributes, id: home.id })
 		);
-		//Génération du HTML
 		let html = "";
 		for (const home of formattedHomes) {
+			//récupère les reviews de la maison
+			const homeReviews = reviewsDatas.data.filter(
+				(review) => review.attributes.home.data.id == home.id
+			);
+
+			//Génération du HTML
 			html += `<li class="card-home w-1/2 md:w-1/4 lg:w-1/5 px-3">
 						<a
 							href="home.html?id=${home.id}"
@@ -27,21 +86,7 @@ fetch("http://localhost:1337/api/homes?populate=*")
 							>
 								<h2>${home.title}</h2>
 								<div class="rating">
-									<span
-										class="mask mask-star-2 bg-slate-100 w-6 aspect-square inline-block"
-									></span>
-									<span
-										class="mask mask-star-2 bg-slate-100 w-6 aspect-square inline-block"
-									></span>
-									<span
-										class="mask mask-star-2 bg-slate-100 w-6 aspect-square inline-block"
-									></span>
-									<span
-										class="mask mask-star-2 bg-slate-100 opacity-50 w-6 aspect-square inline-block"
-									></span>
-									<span
-										class="mask mask-star-2 bg-slate-100 opacity-50 w-6 aspect-square inline-block"
-									></span>
+									${generateRating(avgReviews(homeReviews))} 
 								</div>
 								<div class="host">
 									<div class="avatar placeholder mr-1">
@@ -62,5 +107,5 @@ fetch("http://localhost:1337/api/homes?populate=*")
 		}
 		//injection du HTML dans la liste UL
 		UL.innerHTML = html;
-		console.log(formattedHomes);
+		console.log(" ==== Maisons formatées ====", formattedHomes);
 	});
